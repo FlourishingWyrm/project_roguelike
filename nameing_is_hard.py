5fb6e594a1e3b40c14086e1f107182e27924328e
FlourishingWyrm <141595341+FlourishingWyrm@users.noreply.github.com>
2026-03-10 14:32:05 +1300

    Add files via upload

--- nameing_is_hard.py
+++ nameing_is_hard.py
@@ -1,24 +1,27 @@
 from tkinter import *
 import time
 import random
-from venv import create
+import os
 from PIL import Image
 from fractions import Fraction
+# future move list
 moves = [
     [10,0,"fisticuffs","attack",10,"doubles next action"],
     [10,30,"psychic","mental",1,"perplexed"],
     []
 ]
+# potential use for dictonary
 name_search={
     "tense":0,
     "mind ray":1,
 
 }
-import os
+
 
 if not os.path.exists("{}.txt".format("game_data")):
+    # creates a file if the file doesn't exist so no additional installs are required
     text_file = open("{}.txt".format("game_data"), "w+")
-
+    # varibles stored in the game data
     presets = ("presetsettings = [1,2,4,6,8,10,11,13,15]\n" #
                "active_game = 'no'\n"
                "players_unlocked = []\n"
@@ -26,6 +29,7 @@ if not os.path.exists("{}.txt".format("game_data")):
                "current_moves = []\n"
                "")
     text = str(presets)
+    # removes the quotes for the code to run
     text = text.strip("")
     open("game_data.txt", "w+").write(text)
 # place variables here
@@ -34,51 +38,66 @@ active_game = ""
 players_unlocked = []
 invintory = []
 current_moves = []
+font = ("arial", 16, "bold")
 # https://stackoverflow.com/questions/4719438/editing-specific-line-in-text-file-in-python
 with open('game_data.txt') as f:
+    # seperates the code by lines and removes the spaces
     para_list = [i.strip() for i in f.readlines()]
     for para in para_list:
+        # sets all the variables
         exec(para)
 class dim():
+    """this is used to convert numbers so the screen is constant regardless of resolution"""
     def __init__(self, root_size:float=...):
         try:
             self.frac = str(Fraction(root.winfo_screenwidth(), root.winfo_screenheight())).split("/")
             self.height = int(self.frac[1])*root_size
             self.width = int(self.frac[0])*root_size
         except TclError:
+            # avoids a error message that occurs when the game is ended
             print("game closed successfully")
             quit()
 class fix():
+    """rounding"""
     def r(self,n):
         return int(round(n))
 class MENU():
     def __init__(self):
-        global presetsettings
 
+        self.buttons = []
+        # makes the screen full
         root.attributes("-fullscreen",True)
-        self.presetsettings = presetsettings
+        # gets the background image
         self.photo = PhotoImage(file='/home/nate/school/long_image.png')
+        # puts the background image
         self.backdrop = Label(root, image=self.photo)
         self.backdrop.place(relx=0.5, rely=0.5,anchor="center")
+        # instates a button list
         self.button_list = [
             ["Play", self.loading_menu,dim(0.052).height],
             ["Menu", self.settings, dim(0.064).height],
             ["Quit", root.destroy,dim(0.076).height],
         ]
-        self.buttons = []
-        print(int(round(dim(2.5).width,0)))
+
         for item in self.button_list:
-            self.button_make = Button(root, text=item[0],font=("arial", 16, "bold"), fg="#FFFFFF",bg="#0057D8",width=fix().r(dim(2.5).width), height=fix().r(dim(0.22).height), command=item[1])
+            # creates the startmenu options
+            self.button_make = Button(root, text=item[0],font=font, fg="#FFFFFF",bg="#0057D8",width=fix().r(dim(2.5).width), height=fix().r(dim(0.22).height), command=item[1])
             self.button_make.place(relx=0.5, rely=item[2],anchor="center")
             self.buttons.append(self.button_make)
     def loading_menu(self):
+        # sets up the clear of all things on the menu
         self.backdrop.destroy()
         for item in self.buttons:
             item.destroy()
-        Loading()
+        if presetsettings[6] == 13:
+            Loading()
+        else:
+            map()
     def settings(self):
-        for i in self.buttons:
-            i.config(state="disabled")
+        settings("menu")
+class settings():
+    def __init__(self,type):
+        self.presetsettings = presetsettings
         self.set_list = []
 
         self.set_back_image = PhotoImage(file='/home/nate/school/setting.png')
@@ -90,21 +109,32 @@ class MENU():
             ["Gameplay",lambda : self.clear_settings_options(0),dim(0.024).height,dim(0.024).width],
             ["  Audio  ",lambda :self.clear_settings_options(1),dim(0.024).height,dim(0.0315).width],
             ["  Extra  ",lambda :self.clear_settings_options(2),dim(0.024).height,dim(0.039).width],
-            ["Close",self.un_setting,dim(0.05).height,dim(0.05).width]
+            ["Close",self.un_setting,dim(0.09).height,dim(0.0402).width]
         ]
+        if type == "game":
+            # gives a quit to title screen option for in game settings
+            self.set_button.append(["Main Menu",lambda :MENU(),dim(0.09).height,dim(0.0235).width])
         self.butts = []
+        # adds the tab settings buttons to a list and places them
         for butt in self.set_button:
-            self.make_butt = Button(root, text=butt[0], font=("arial", 16, "bold"),fg="#FFFFFF",bg="#604330", height=fix().r(dim(0.2).height),command=butt[1],padx=fix().r(dim(1.5).width))
+            self.make_butt = Button(root, text=butt[0], font=font,fg="#FFFFFF",bg="#604330", height=fix().r(dim(0.2).height),command=butt[1],padx=fix().r(dim(1.5).width))
             self.make_butt.place(relx=butt[3], rely=butt[2],anchor="center")
             self.butts.append(self.make_butt)
             self.set_list.append(self.make_butt)
         self.current_set_tab = []
         self.clear_settings_options(0)
+        if type == "game":
+            # makes the menu button close the menu if the menu is open in the game section
+            self.make_butt = Button(bg="#604330", width=fix().r(dim(0.6).width), height=fix().r(dim(0.6).height),command=lambda: self.un_setting())
+            self.make_butt.place(relx=dim(0.0025).width, rely=dim(0.009).height, anchor="center")
+            self.set_list.append(self.make_butt)
     def clear_settings_options(self,tab):
+        # removes all things in the current display menu
         for item in self.current_set_tab:
             item.destroy()
         for item in self.butts:
             if item == self.butts[tab]:
+                # disables the current tab
                 self.butts[tab].config(state="disabled")
             else:
                 item.config(state="normal")
@@ -119,10 +149,11 @@ class MENU():
             ["Credits                                     ", 14.3, 14.9, dim(0.065).height, self.presetsettings[7]],
             ["Reset Settings                              ", 16.3, 16.9, dim(0.085).height, self.presetsettings[8]]
         ][tab*3:tab*3+3]:
+            # takes the current items in the tab and displays them
             self.current_set_tab_option = Scale(root, from_=item[1], to=item[2], orient="horizontal", showvalue=False,
                                                 command=self.change_setting, variable=IntVar(value=item[4]))
             self.current_set_tab_option.place(relx=0.6, rely=item[3], anchor="center")
-            self.current_set_tab_option_text = Label(text=item[0], font=("arial", 16, "bold"), fg="#FFFFFF",
+            self.current_set_tab_option_text = Label(text=item[0], font=font, fg="#FFFFFF",
                                                      bg="#754525", justify="left")
             self.current_set_tab_option_text.place(relx=0.45, rely=item[3], anchor="center", )
             self.current_set_tab.append(self.current_set_tab_option)
@@ -130,16 +161,14 @@ class MENU():
         for item in self.current_set_tab:
             self.set_list.append(item)
     def change_setting(self,change):
+        # edits the setting and writes the edit to file
         source = round(float(change)/2.1)
         change = int(change)
-        print(source)
-        print(change)
         with open('game_data.txt', 'r') as file:
             # read a list of lines into data
             data = file.readlines()
         if source == 8:
             self.presetsettings = [1,2,5,7,9,11,13,14,16]
-            print(self.presetsettings)
             self.clear_settings_options(2)
             # and write everything back
         else :
@@ -149,10 +178,10 @@ class MENU():
             file.writelines(data)
 
     def un_setting(self):
+        # closes the menu
         for item in self.set_list:
             item.destroy()
-        for i in self.buttons:
-            i.config(state="normal")
+
 
 
 
@@ -164,7 +193,7 @@ class Loading():
         # gets the file for the background of the gif
         self.photo = PhotoImage(file="bar.gif")
         # defines the canvas and its background
-        self.canvas = Canvas(root, width=1920, height=1080,bg="#262626")
+        self.canvas = Canvas(root, width=dim(120).width, height=dim(120).height,bg="#262626")
         self.canvas.pack(fill="both", expand=True)
 
 
@@ -172,18 +201,15 @@ class Loading():
         self.info = Image.open('/home/nate/school/bar.gif')
         # sets the frames
         self.frames = self.info.n_frames
-        print(self.frames)
         self.photoimage_objects = []
         # stores the invidual frames
         # starts the animation
         self.frames-=1
-        print(self.frames)
         self.animation(1)
         root.mainloop()
     def animation(self,current_frame=0):
         global loop
         self.image = PhotoImage(file='/home/nate/school/bar.gif', format=f'gif -index {current_frame}')
-        print(current_frame)
         # reprints background and then foreground overtop
         self.canvas.create_image(950, 860, image=self.photo)
         self.canvas.create_image(950, 860, image=self.image)
@@ -192,15 +218,46 @@ class Loading():
 
         if current_frame == self.frames:
             self.canvas.destroy()
-            startgame()
+            # loads the map
+            map()
             root.mainloop()
 
 
         loop = root.after(random.randint(60, 150), lambda: self.animation(current_frame))
 
-class startgame():
+class map():
+    # will allow players to select options for the story
     def __init__(self):
-        self.frame = Frame(padx=0, pady=0)
+        self.canvas = Canvas(root, width=dim(120).width, height=dim(120).height, bg="#262626")
+        self.canvas.pack(fill="both", expand=True)
+        # temparary load button
+        self.level_load_button = Button(root,text="Battle",font=font, fg="#FFFFFF",bg="#0057D8",width=fix().r(dim(2.5).width), height=fix().r(dim(0.22).height), command=lambda: self.load_level())
+        self.level_load_button.place(relx=0.5,rely=0.5,anchor="center")
+    def load_level(self):
+        # preps the level
+        self.canvas.destroy()
+        self.level_load_button.destroy()
+        load_level()
+        menu_bar()
+class load_level():
+    def __init__(self):
+        # the level
+        self.canvas = Canvas(root, width=dim(120).width, height=dim(120).height, bg="#262626")
+        self.canvas.pack(fill="both", expand=True)
+class menu_bar():
+    # a bar for the menu
+    def __init__(self):
+        self.canvas = Canvas(root, width=dim(120).width, height=180, bg="#754525")
+        self.canvas.pack(fill="both")
+        self.menu_bar = Label(bg="#754525",width=dim(120).width,height=fix().r(dim(0.75).height))
+        self.menu_bar.place(relx=0.5, rely=dim(0.009).height, anchor="center")
+        self.menu_button = Button(bg="#604330",width=fix().r(dim(0.6).width),height=fix().r(dim(0.6).height),command=lambda: settings("game"))
+        self.menu_button.place(relx=dim(0.0025).width, rely=dim(0.009).height, anchor="center")
+
+
+
+
+
 
 if __name__ == '__main__':
     """who knows what this does"""
@@ -208,6 +265,7 @@ if __name__ == '__main__':
     root.title("Colour Quest")
     MENU()
     root.mainloop()
+# settings
 a = [
 ["Skip battle animations                      ", 0, 0.9, dim(0.04).height],
 ["Auto end turn                               ", 2, 2.5, dim(0.06).height],
